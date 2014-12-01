d15481481422e658e55e17f53b100da566ccbc89
haolei <728064001@qq.com>
2014-12-02 00:51:32 +0800

    create nav

---
+++ view/js/custom.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+//固定导航栏
+  //initializing jQuery
+  jQuery(function($) {
+	$(document).ready( function() {
+	  //enabling stickUp on the '.navbar-wrapper' class
+	  $('#nav').stickUp({
+		//enabling marginTop with the 'auto' setting 
+		marginTop: 'auto'
+	  });
+  });
+});
+              
